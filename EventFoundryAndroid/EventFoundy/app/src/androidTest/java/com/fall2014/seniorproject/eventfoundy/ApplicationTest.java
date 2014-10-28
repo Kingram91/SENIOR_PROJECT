9f822000699121e57e0260bcb57de03e0944bb2f
James W. Kimani <jkimani2@gmail.com>
2014-10-28 15:58:37 -0400

    delete unused files

---
+++ EventFoundryAndroid/EventFoundy/app/src/androidTest/java/com/fall2014/seniorproject/eventfoundy/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.fall2014.seniorproject.eventfoundy;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
